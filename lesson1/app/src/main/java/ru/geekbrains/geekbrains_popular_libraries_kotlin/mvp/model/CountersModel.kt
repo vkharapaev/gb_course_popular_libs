e0ce4f848bfdb976080ee15b7089a154f00a9118
Харапаев Владимир <vharapaev@rbc.ru>
2021-07-10 11:42:25 +0300

    initial

---
+++ lesson1/app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/model/CountersModel.kt
@@ -0,0 +1,20 @@
+package ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model
+
+class CountersModel {
+
+    val counters = mutableListOf(0, 0, 0)
+
+    fun getCurrent(index: Int): Int {
+        return counters[index]
+    }
+
+    fun next(index: Int): Int{
+        counters[index] ++
+        return getCurrent(index)
+    }
+
+    fun set(index: Int, value: Int){
+        counters[index] = value
+    }
+
+}
