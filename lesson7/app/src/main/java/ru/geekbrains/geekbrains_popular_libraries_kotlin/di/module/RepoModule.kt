6b6a62eae1e8e32ac58a6f3c7977e22221325ae3
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-08-04 01:14:00 +0300

    hw7

--- lesson7/app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/di/module/RepoModule.kt
+++ lesson7/app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/di/module/RepoModule.kt
@@ -3,9 +3,12 @@ package ru.geekbrains.geekbrains_popular_libraries_kotlin.di.module
 import dagger.Module
 import dagger.Provides
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.api.IDataSource
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.cache.IGithubRepositoriesCache
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.cache.IGithubUsersCache
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.network.INetworkStatus
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.repo.IGithubRepositoriesRepo
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.repo.IGithubUsersRepo
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.repo.RetrofitGithubRepositoriesRepo
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.repo.RetrofitGithubUsersRepo
 import javax.inject.Singleton
 
@@ -20,5 +23,14 @@ class RepoModule {
         cache: IGithubUsersCache
     ): IGithubUsersRepo =
         RetrofitGithubUsersRepo(api, networkStatus, cache)
+
+    @Singleton
+    @Provides
+    fun provideRepositoriesRepo(
+        api: IDataSource,
+        network: INetworkStatus,
+        cache: IGithubRepositoriesCache
+    ): IGithubRepositoriesRepo =
+        RetrofitGithubRepositoriesRepo(api, network, cache)
 
 }
