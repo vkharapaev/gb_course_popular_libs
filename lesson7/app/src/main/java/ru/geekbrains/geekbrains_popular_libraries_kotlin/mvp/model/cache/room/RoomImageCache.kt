5fe50142959ae4910e36659a6f6b31410b1e0204
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-08-04 01:00:19 +0300

    hw7

--- lesson7/app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/model/cache/room/RoomImageCache.kt
+++ lesson7/app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/model/cache/room/RoomImageCache.kt
@@ -6,18 +6,18 @@ import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.cache.IImageCache
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.entity.room.RoomCachedImage
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.entity.room.db.Database
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.entity.room.dao.ImageDao
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.security.MessageDigest
 
-class RoomImageCache(val db: Database, val dir: File) : IImageCache {
+class RoomImageCache(private val imageDao: ImageDao, val dir: File) : IImageCache {
     private fun String.md5() = hash("MD5")
     private fun String.hash(algorithm: String) = MessageDigest.getInstance(algorithm).digest(toByteArray()).fold("", { str, it -> "%02x".format(it) })
 
     override fun getBytes(url: String) = Maybe.fromCallable {
-        db.imageDao.findByUrl(url)?.let {
+        imageDao.findByUrl(url)?.let {
             File(it.localPath).inputStream().readBytes()
         }
     }.subscribeOn(Schedulers.io())
@@ -37,14 +37,14 @@ class RoomImageCache(val db: Database, val dir: File) : IImageCache {
         } catch (e: Exception) {
             emitter.onError(e)
         }
-        db.imageDao.insert(RoomCachedImage(url, imageFile.path))
+        imageDao.insert(RoomCachedImage(url, imageFile.path))
         emitter.onComplete()
     }.subscribeOn(Schedulers.io())
 
-    override fun contains(url: String) = Single.fromCallable { db.imageDao.findByUrl(url) != null }.subscribeOn(Schedulers.io())
+    override fun contains(url: String) = Single.fromCallable { imageDao.findByUrl(url) != null }.subscribeOn(Schedulers.io())
 
     override fun clear() = Completable.fromAction {
-        db.imageDao.clear()
+        imageDao.clear()
         dir.deleteRecursively()
     }.subscribeOn(Schedulers.io())
 
