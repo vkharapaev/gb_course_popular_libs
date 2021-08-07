add962416d0354bf4691e887e02b546a319b32a3
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-08-07 16:05:04 +0300

    commit

---
+++ course-project/find-movie-2/app/src/main/java/com/headmostlab/findmovie2/ui/App.kt
@@ -0,0 +1,6 @@
+package com.headmostlab.findmovie2.ui
+
+import android.app.Application
+
+class App: Application() {
+}
--- course-project/find-movie-2/app/src/main/java/com/headmostlab/findmovie2/ui/activity/MainActivity.kt
+++ course-project/find-movie-2/app/src/main/java/com/headmostlab/findmovie2/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.headmostlab.findmovie2
+package com.headmostlab.findmovie2.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.headmostlab.findmovie2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
